d6a807d70274383445ec55e5be80562729103184
Christofer Johnson <christofer.d.johnson@gmail.com>
2017-11-14 14:31:29 +0000

    automatically backed up by learn

--- lib/oo_multiples.rb
+++ lib/oo_multiples.rb
@@ -6,13 +6,14 @@ class Multiples
   end
 
   def collect_multiples
-    sum = []
-    for i in (1...@limit) do
-      if i%3==0 || i%5==0
-        sum << i
-      end
-    end
-    sum
+    # sum = []
+    # for i in (1...@limit) do
+    #   if i%3==0 || i%5==0
+    #     sum << i
+    #   end
+    # end
+    # sum
+    (1...limit).select{|i| i%3==0 || i%5==0}
   end
 
   def sum_multiples
